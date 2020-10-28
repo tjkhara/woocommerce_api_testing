fe9c4c9e7a7337c7c2f5937d7d06f297b1fd5199
Tajeshwar Khara <tkhara@gmail.com>
2020-10-28 18:59:46 +0530

    Code after section 4 video 29. Created random email and password generator.

--- ssqaapitest/tests/customers/test_create_customers_smoke.py
+++ ssqaapitest/tests/customers/test_create_customers_smoke.py
@@ -1,16 +1,23 @@
 import pytest
 import logging as logger
+from ssqaapitest.src.utilities.generic_utilities import generate_random_email_and_password
 
 @pytest.mark.tcid29
 def test_create_customer_only_email_password():
 
     logger.info("TEST: Create new customer with email and password only.")
-    email = ""
-    password = ""
+
+    rand_info = generate_random_email_and_password()
+    logger.info(rand_info)
+
+    email = rand_info.get("email")
+    password = rand_info.get("password")
 
     # create payload
+    payload = {"email":email, "password":password}
 
     # make the call
+
 
     # verify status code of the call
 
