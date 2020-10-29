0a45459ad4b67f22a41a79679d9999297fa574ae
Tajeshwar Khara <tkhara@gmail.com>
2020-10-29 15:44:41 +0530

    TCID-47 is done. Code is working fine. Just changed the number of the test case the id is TCID30

---
+++ ssqaapitest/tests/customers/test_get_customers_smoke.py
@@ -0,0 +1,9 @@
+import pytest
+from ssqaapitest.src.utilities.requests_utility import RequestsUtility
+import logging as logger
+
+@pytest.mark.tcid30
+def test_get_all_customers():
+    request_helper = RequestsUtility()
+    response_api = request_helper.get('customers')
+    assert response_api, f"Response of list all customers is empty."
